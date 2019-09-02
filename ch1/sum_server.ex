6a9dbca08ea4711bd1ac5313458114507d65afea
Matt Willy <matt.willy@gmail.com>
2019-09-02 12:19:23 -0600

    same code, but in elixir

---
+++ ch1/sum_server.ex
@@ -0,0 +1,19 @@
+defmodule SumServer do
+  use GenServer
+
+  def init(_) do
+    {:ok, nil}
+  end
+
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def sum(server, a, b) do
+    GenServer.call(server, {:sum, a, b})
+  end
+
+  def handle_call({:sum, a, b}, _from, state) do
+    {:reply, a + b, state}
+  end
+end
