42cbf076a9930d0c98a36add594b1f22ba9b49e5
Matt Willy <matt.willy@gmail.com>
2019-09-20 17:08:13 -0600

    calculate the length of a list

--- ch3/list_helper.ex
+++ ch3/list_helper.ex
@@ -3,6 +3,12 @@ defmodule ListHelper do
     do_sum(0, list)
   end
 
+  def list_len([]), do: 0
+
+  def list_len([_head|tail]) do
+    1 + list_len(tail)
+  end
+
   defp do_sum(current_sum, []) do
     current_sum
   end
