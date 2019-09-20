1b592d88c7cd5598786a3b88a0385c16d1ad6c18
Matt Willy <matt.willy@gmail.com>
2019-09-20 17:20:18 -0600

    create a function for creating a range

--- ch3/list_helper.ex
+++ ch3/list_helper.ex
@@ -4,10 +4,15 @@ defmodule ListHelper do
   end
 
   def list_len([]), do: 0
-
   def list_len([_head|tail]) do
     1 + list_len(tail)
   end
+
+  def range(from, to) when from < to do
+    range(from, to, [])
+  end 
+  
+  def range(from, to), do: Enum.reverse(range(to, from))
 
   defp do_sum(current_sum, []) do
     current_sum
@@ -15,5 +20,10 @@ defmodule ListHelper do
 
   defp do_sum(current_sum, [head | tail]) do
     do_sum(current_sum + head, tail)
+  end
+
+  defp range(to, to, list), do: list ++ [to]
+  defp range(from, to, list) do
+    range(from+1, to, list ++ [from]) 
   end
 end
