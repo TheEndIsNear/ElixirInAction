93f378ab71333791dc90ef919d6f400e159bb51a
Matt Willy <matt.willy@gmail.com>
2020-03-05 20:01:07 -0700

    register the todo_server process to make the api easier to use

--- ch5/todo_server.ex
+++ ch5/todo_server.ex
@@ -1,22 +1,24 @@
 defmodule TodoServer do
+  
   def start do
-    spawn(fn -> loop(TodoList.new()) end)
+    pid = spawn(fn -> loop(TodoList.new()) end)
+    Process.register(pid, :todo_server)
   end
 
-  def add_entry(todo_server, new_entry) do
-    send(todo_server, {:add_entry, new_entry})
+  def add_entry(new_entry) do
+    send(:todo_server, {:add_entry, new_entry})
   end
 
-  def update_entry(todo_server, entry, update_func) do
-    send(todo_server, {:update_entry, entry, update_func})
+  def update_entry(entry, update_func) do
+    send(:todo_server, {:update_entry, entry, update_func})
   end
 
-  def delete_entry(todo_server, entry_id) do
-    send(todo_server, {:delete_entry, entry_id})
+  def delete_entry(entry_id) do
+    send(:todo_server, {:delete_entry, entry_id})
   end
   
-  def entries(todo_server, date) do
-    send(todo_server, {:entries, self(), date})
+  def entries(date) do
+    send(:todo_server, {:entries, self(), date})
 
     receive do
       {:todo_entries, entries} -> entries
@@ -28,7 +30,8 @@ defmodule TodoServer do
   defp loop(todo_list) do
     new_todo_list =
       receive do
-        message -> process_message(todo_list, message)
+        message ->
+           process_message(todo_list, message)
       end
 
     loop(new_todo_list)
@@ -47,6 +50,7 @@ defmodule TodoServer do
   end
 
   defp process_message(todo_list, {:entries, caller, date}) do
+    IO.puts("Do I get here")
     send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
     todo_list
   end
