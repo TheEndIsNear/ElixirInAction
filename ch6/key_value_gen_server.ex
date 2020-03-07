21de960da7556e656bac8e84d95d9127683431b0
Matt Willy <matt.willy@gmail.com>
2020-03-06 17:29:31 -0700

    add @impl true to get compile safety

--- ch6/key_value_gen_server.ex
+++ ch6/key_value_gen_server.ex
@@ -2,11 +2,8 @@ defmodule KeyValueStore do
   use GenServer
 
   def start do
+    :timer.send_interval(5000, :cleanup)
     GenServer.start(KeyValueStore, nil)
-  end
-
-  def init(_) do
-    {:ok, %{}}
   end
 
   def put(pid, key, value) do
@@ -17,12 +14,24 @@ defmodule KeyValueStore do
     GenServer.call(pid, {:get, key})
   end
 
+  @impl true
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  @impl true
   def handle_cast({:put, key, value}, state) do
      {:noreply, Map.put(state, key, value)}
   end
 
+  @impl true
   def handle_call({:get, key}, _, state) do
     {:reply, Map.get(state, key), state}
   end
-end
 
+  @impl true
+  def handle_info(:cleanup, state) do
+    IO.puts("performing cleanup...")
+    {:noreply, state}
+  end
+end
